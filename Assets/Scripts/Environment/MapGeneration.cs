7239918e12eef73007a3d0940ff129fa7d76077f
Jorge <jgaliano2000@gmail.com>
2021-11-16 19:39:50 +0100

    [ENVIROMENT][MAP] Objetos aleatorios
    Los objetos se generan de forma aleatoria en el mapa sin afectar al grid system y hya algunos objetos fijos

--- Assets/Scripts/Environment/MapGeneration.cs
+++ Assets/Scripts/Environment/MapGeneration.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class MapGeneration : MonoBehaviour
 {
     public GameObject[] objPosibles;
+    private Quaternion auxRot = Quaternion.identity;
+    int aux;
     // Start is called before the first frame update
     void Start()
     {
+
+        aux = Random.Range(0, objPosibles.Length);
+
         Instantiate(
-            objPosibles[Random.Range(0, objPosibles.Length)],
+            objPosibles[aux],
             transform.position,
-            Quaternion.identity
+            auxRot = (aux == 0 || aux == 1 || aux == 2) ? Quaternion.Euler(-90, 0, 0) : Quaternion.identity
             );
     }
+
 
     // Update is called once per frame
     void Update()
