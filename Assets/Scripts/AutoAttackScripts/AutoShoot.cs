52da2eb0e8290e024b61a1e57458e72f8c58d655
Bozhidar Petrov Valchev <acbossy@gmail.com>
2021-11-17 06:48:45 +0100

    [HotFix] Añadida una variable para depurar el daño del jugador
    Se ha añadido una variable para cambiar a mano, el daño en tiempo de ejecución.

--- Assets/Scripts/AutoAttackScripts/AutoShoot.cs
+++ Assets/Scripts/AutoAttackScripts/AutoShoot.cs
@@ -33,6 +33,9 @@ namespace AutoAttackScripts
         /// </summary>
         private float localDistance = 0;
 
+        [Header("Change this to set a new Damage to the player")] [SerializeField]
+        private float _damagePerBullet;
+
         private Vector3 vectorAb;
 
         private bool shooting = false;
@@ -60,6 +63,12 @@ namespace AutoAttackScripts
         {
             get => shooting;
             set => shooting = value;
+        }
+
+        public float DamagePerBullet
+        {
+            get => _damagePerBullet;
+            set { _damagePerBullet = value; }
         }
 
         public enum WhoToShoot
@@ -141,6 +150,10 @@ namespace AutoAttackScripts
             quadrant = new bool[2];
         }
 
+        private void Update()
+        {
+            PlayerStats._instance.Damage = DamagePerBullet;
+        }
 
         private void OnCollisionExit(Collision other)
         {
@@ -415,7 +428,7 @@ namespace AutoAttackScripts
 
         private void ShootingNormal(GameObject enemy)
         {
-            PlayerStats._instance.Damage = 3;
+            //PlayerStats._instance.Damage = 3;
             //calculate direction from the attackpoint to the enemy
             var position = attackPoint.position;
             Vector3 directionShoot = enemy.transform.position - position;
@@ -431,14 +444,14 @@ namespace AutoAttackScripts
 
         private void ShootingBurst(GameObject enemy)
         {
-            PlayerStats._instance.Damage = 2;
+            //PlayerStats._instance.Damage = 2;
             ShootBulletBurst(enemy);
             StartCoroutine(nameof(WaitToShootBulletBurst), enemy);
         }
 
         private void ShootingShotgun(GameObject enemy)
         {
-            PlayerStats._instance.Damage = 10;
+            //PlayerStats._instance.Damage = 10;
             var enemyPos = enemy.transform.position;
             var position = attackPoint.position;
 
