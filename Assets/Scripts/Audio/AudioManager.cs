35dda9514395664295162d29a82d6155b4064772
Bozhidar Petrov Valchev <acbossy@gmail.com>
2021-11-18 21:11:40 +0100

    [Sonido] Se ha creado un AudioManager
    Se ha creado un AudioManager que se encarga de manejar el audio y el sonido de la escena y se han incluido los sonidos y la música del menú principal

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,65 @@
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    public static AudioManager instance;
+
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        if (instance == null || instance != this)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+        foreach (var s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+
+    private void Start()
+    {
+        Play("Menu Principal");
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found");
+            return;
+        }
+
+        s.source.Play();
+    }
+
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found");
+            return;
+        }
+
+        s.source.Stop();
+    }
+}
---
+++ Assets/Scripts/Audio/Sound.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+    [Range(0f, 1f)] public float volume;
+    [Range(.1f, 3f)] public float pitch;
+    public bool loop;
+    [HideInInspector] public AudioSource source;
+}
