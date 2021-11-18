cfac4cd6109d1823e6c8609b5f1ff01e706a6b4a
Bozhidar Petrov Valchev <acbossy@gmail.com>
2021-11-18 21:23:15 +0100

    [Sonido] He creado dos audiomixers
    Para tener un control general de la música y los SFX.

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -26,7 +26,7 @@ public class AudioManager : MonoBehaviour
         {
             s.source = gameObject.AddComponent<AudioSource>();
             s.source.clip = s.clip;
-
+            s.source.outputAudioMixerGroup = s.output;
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
             s.source.loop = s.loop;
--- Assets/Scripts/Audio/Sound.cs
+++ Assets/Scripts/Audio/Sound.cs
@@ -6,6 +6,7 @@ using UnityEngine.Audio;
 public class Sound
 {
     public string name;
+    public AudioMixerGroup output;
     public AudioClip clip;
     [Range(0f, 1f)] public float volume;
     [Range(.1f, 3f)] public float pitch;
