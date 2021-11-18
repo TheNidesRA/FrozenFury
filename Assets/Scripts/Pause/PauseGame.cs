db878111f566fb1ad53c3fd1faeb209fda3b77e1
Elnidas <pedroydani1@hotmail.com>
2021-11-18 20:11:50 +0100

    [Menu] Pause
    Se puede pausar el juego y volver al menu principal

---
+++ Assets/Scripts/Pause/PauseGame.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+  public static void PauseGameAction()
+  {
+    Time.timeScale = 0;
+  }
+  
+  public static void ResumeGameAction()
+  {
+    Time.timeScale = 1;
+  }
+
+  
+  
+}
