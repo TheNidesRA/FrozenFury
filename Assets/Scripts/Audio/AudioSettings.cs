81aae96b8afbd9e9349646d6187b4f1d05a3cb6c
Bozhidar Petrov Valchev <acbossy@gmail.com>
2021-11-21 10:07:12 +0100

    [Sonido] Se ha creado un menu de música
    Ya existe un menu para ajustar los sonidos y la música

---
+++ Assets/Scripts/Audio/AudioSettings.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioSettings : MonoBehaviour
+{
+    public AudioMixer soundMixer;
+    public AudioMixer musicMixer;
+
+    public void SetVolumeSounds(float volume)
+    {
+        if (volume == 0)
+        {
+            soundMixer.SetFloat("volume", Mathf.Log10(0.01f) * 20);
+            return;
+        }
+
+        soundMixer.SetFloat("volume", Mathf.Log10(volume) * 20);
+    }
+
+    public void SetVolumeMusic(float volume)
+    {
+        if (volume == 0)
+        {
+            soundMixer.SetFloat("volume", Mathf.Log10(0.01f) * 20);
+            return;
+        }
+
+        musicMixer.SetFloat("volume", Mathf.Log10(volume) * 20);
+    }
+}
