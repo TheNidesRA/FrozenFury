ec3d67ef70e49f135730b42a52f102aaed8c0c84
Bozhidar Petrov Valchev <acbossy@gmail.com>
2021-11-21 13:18:13 +0100

    [Sonido] Se ha mejorado el layout de los sonidos
    Ahora se puede mutear y desmutear tanto la música como los sonidos dándole al botón

--- Assets/Scripts/Audio/AudioSettings.cs
+++ Assets/Scripts/Audio/AudioSettings.cs
@@ -3,11 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioSettings : MonoBehaviour
 {
     public AudioMixer soundMixer;
     public AudioMixer musicMixer;
+    public Slider musicSlider;
+    public Slider audioSlider;
+    public bool isMutedAudio = false;
+    public bool isMutedMusic = false;
+    public float previousAudio = 0.5f;
+    public float previousSliderMusic = 0.5f;
+    public float previousSliderAudio = 0.5f;
+    public float previousMusic = 0.5f;
 
     public void SetVolumeSounds(float volume)
     {
@@ -18,6 +27,7 @@ public class AudioSettings : MonoBehaviour
         }
 
         soundMixer.SetFloat("volume", Mathf.Log10(volume) * 20);
+        previousAudio = Mathf.Log10(volume) * 20;
     }
 
     public void SetVolumeMusic(float volume)
@@ -29,5 +39,42 @@ public class AudioSettings : MonoBehaviour
         }
 
         musicMixer.SetFloat("volume", Mathf.Log10(volume) * 20);
+        previousMusic = Mathf.Log10(volume) * 20;
+    }
+
+    public void MuteAudio()
+    {
+        if (!isMutedAudio)
+        {
+            isMutedAudio = true;
+            previousSliderAudio = audioSlider.value;
+            audioSlider.value = 0;
+            SetVolumeSounds(0);
+        }
+        else
+        {
+            isMutedAudio = false;
+            SetVolumeSounds(previousAudio);
+            audioSlider.value = previousSliderAudio;
+            previousSliderAudio = 0;
+        }
+    }
+
+    public void MuteMusic()
+    {
+        if (!isMutedMusic)
+        {
+            isMutedMusic = true;
+            previousSliderMusic = musicSlider.value;
+            musicSlider.value = 0;
+            SetVolumeMusic(0);
+        }
+        else
+        {
+            isMutedMusic = false;
+            SetVolumeMusic(previousMusic);
+            musicSlider.value = previousSliderMusic;
+            previousSliderMusic = 0;
+        }
     }
 }
