2189f485d8d268ff6279fb1a5b042997c1816b22
Elnidas <pedroydani1@hotmail.com>
2021-11-23 13:05:19 +0100

    [UI] fixed los problemas con #1,2 y 3

--- Assets/Scripts/Layout/MainLayout/ButtonWithTextAnimation.cs
+++ Assets/Scripts/Layout/MainLayout/ButtonWithTextAnimation.cs
@@ -5,32 +5,41 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class ButtonWithTextAnimation : MonoBehaviour,IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
+public class ButtonWithTextAnimation : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
 {
-
     private RectTransform _rcTransform;
-    public float increase=1.2f;
-    public float time=0.3f;
+    public float increase = 1.2f;
+    public float time = 0.3f;
     public GameObject text;
     public string description;
+
 
     private TextMeshProUGUI _text;
     private Vector2 _initialSize;
     private LTDescr _animation;
+    private Button btn = null;
+    public bool dishableOnPress;
 
     private void Awake()
     {
-        if(text!=null)
+        if (text != null)
             _text = text.GetComponent<TextMeshProUGUI>();
         _rcTransform = GetComponent<RectTransform>();
         _initialSize = new Vector2(_rcTransform.rect.width, _rcTransform.rect.height);
+        TryGetComponent(out btn);
     }
 
 
     private void OnEnable()
     {
         _rcTransform.sizeDelta = _initialSize;
+        if (!ReferenceEquals(btn, null))
+        {
+            if (!btn.interactable)
+                btn.interactable = true;
+        }
     }
 
     private void OnDisable()
@@ -40,21 +49,25 @@ public class ButtonWithTextAnimation : MonoBehaviour,IPointerEnterHandler, IPoin
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-       
-        _animation= LeanTween.size(_rcTransform, _initialSize * increase, time);
-        if(_text!=null)
-        _text.text = description;
+        _animation = LeanTween.size(_rcTransform, _initialSize * increase, time);
+        if (_text != null)
+            _text.text = description;
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        
+        if (dishableOnPress && !ReferenceEquals(btn, null))
+        {
+            if (btn.interactable)
+                btn.onClick.Invoke();
+            btn.interactable = false;
+        }
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        _animation=  LeanTween.size(_rcTransform, _initialSize,time);
-        if(_text!=null)
-        _text.text = "";
+        _animation = LeanTween.size(_rcTransform, _initialSize, time);
+        if (_text != null)
+            _text.text = "";
     }
 }
