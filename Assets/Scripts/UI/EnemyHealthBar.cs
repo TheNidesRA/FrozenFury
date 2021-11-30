8d01902b98a28fca00bb63ac21f708ec8750f0cc
MiguelVDP <miguelvidaldelaplaza@gmail.com>
2021-11-30 21:06:34 +0100

    [UI]Health Bar de enemigos añadida
    Se ha añadido healthbar a los enemigos

--- Assets/Scripts/UI/EnemyHealthBar.cs
+++ Assets/Scripts/UI/EnemyHealthBar.cs
@@ -1,4 +1,55 @@
-﻿namespace UI
+﻿using System;
+using Enemies;
+using UnityEngine;
+using UnityEngine.Rendering.VirtualTexturing;
+using UnityEngine.XR;
+
+namespace UI
 {
-    public class EnemyHealthBar
+    public class EnemyHealthBar : MonoBehaviour
+    {
+        private float maxHealth;
+        public UnityEngine.UI.Image healtBar;
+
+        private float fixedRotation = 0;
+
+        private Quaternion fixedrot;
+
+        private void Awake()
+        {
+            Vector3 initPos = gameObject.transform.position;
+            Vector3 aux = initPos;
+            Vector3 camPos = Camera.main.transform.position;
+            aux.x = camPos.x;
+            
+            gameObject.transform.position = aux;
+            gameObject.transform.LookAt(camPos);
+            fixedrot = gameObject.transform.rotation;
+            gameObject.transform.position = initPos;
+            GetComponentInParent<Enemy>().HealthBarEvent += HandleHealth;
+        }
+        
+        private void Update()
+        {
+            gameObject.transform.rotation = fixedrot;
+        }
+
+        private void OnEnable()
+        {
+            maxHealth = GetComponentInParent<Enemy>().maxHealth;
+        }
+
+        private void OnDisable()
+        {
+            //Esto tira fallo si se para la partida y hay enemigos en pantalla
+            GetComponentInParent<Enemy>().HealthBarEvent -= HandleHealth;
+            Debug.Log("Me desuscribo illo");
+        }
+
+        private void HandleHealth(object e, float health)
+        {
+            healtBar.fillAmount = health / maxHealth;
+        }
+
+    }
 }
