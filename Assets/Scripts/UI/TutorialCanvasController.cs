7fca79819796e235be3d7a6bd4215e3d0a786bc7
Jorge <jgaliano2000@gmail.com>
2021-12-02 21:23:18 +0100

    [TUTORIAL] Ajustes
    Se ha cambiado el nombre del script y se ha modificado la escena.

--- Assets/Scripts/UI/TutorialCanvasController.cs
+++ Assets/Scripts/UI/TutorialCanvasController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CharacterChooseCanvas : MonoBehaviour
+public class TutorialCanvasController : MonoBehaviour
 {
     public RectTransform canvas;
     public int index;
