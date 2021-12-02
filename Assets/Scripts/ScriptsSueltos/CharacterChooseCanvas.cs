9d388cbb0e0acf5eb9e022ddd0136e85435dfa5a
Jorge <jgaliano2000@gmail.com>
2021-12-02 20:46:34 +0100

    [TUTORIAL] Tutorial funcional
    Ahora el tutorial es funcional, se han incluido puntos de navegación y flechas para desplazarse por los menús.

--- Assets/Scripts/ScriptsSueltos/CharacterChooseCanvas.cs
+++ Assets/Scripts/ScriptsSueltos/CharacterChooseCanvas.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,14 +9,46 @@ public class CharacterChooseCanvas : MonoBehaviour
     public int index;
     public float time = 0.8f;
     public AnimationCurve curve;
-    public GameObject botonIzq, botonDcho;
+    public List<GameObject> botones;
     public GameObject[] paginasTutorial;
+
+    private event Action OnLeanComplete;
     // Start is called before the first frame update
     void Start()
     {
         index = 0;
-        botonDcho.SetActive(true);
-        botonIzq.SetActive(false);
+        botones[1].SetActive(true);
+        botones[0].SetActive(false);
+    }
+
+
+    private void reactivarBotones()
+    {
+        foreach(GameObject boton in botones)
+        {
+            boton.SetActive(true);
+        }
+    }
+
+    private void desactivarBotones()
+    {
+        foreach (GameObject boton in botones)
+        {
+            boton.SetActive(false);
+        }
+    }
+
+    private void flechasInicioYfin()
+    {
+        if (index == 0)
+            botones[1].SetActive(false);
+        else if (index > 0)
+            botones[0].SetActive(true);
+
+        if (index == 7)
+            botones[0].SetActive(false);
+        else if (index < 7)
+            botones[1].SetActive(true);
     }
 
     public void MoveRight()
@@ -25,25 +58,55 @@ public class CharacterChooseCanvas : MonoBehaviour
         //
         // Debug.Log("X : "+ xOriginal + " "+mov);
         // LeanTween.moveX(canvas, -mov,time).setEase(curve);
-
+        desactivarBotones();
         float x = canvas.localPosition.x;
-        canvas.LeanMoveX(x+-1920,time).setEase(curve);
+        canvas.LeanMoveX(x + -1920, time).setEase(curve).setOnComplete(()=>{ if(index == 0 || index == 7) flechasInicioYfin(); else reactivarBotones();});
         index++;
     }
 
     public void MoveLeft()
     {
-
+        desactivarBotones();
         float x = canvas.localPosition.x;
-        canvas.LeanMoveX(x+1920,time).setEase(curve);
+        canvas.LeanMoveX(x+1920,time).setEase(curve).setOnComplete(() => { if (index == 0 || index == 7) flechasInicioYfin(); else reactivarBotones(); });
         index--;
     }
     
+    public void MoveThrough(int i)
+    {
+        
+
+        if(i < index) //Caso en el que el boton pulsado es menor que el índice todavía  
+        {
+            flechasInicioYfin();
+            float x = canvas.localPosition.x;
+            Debug.Log(i + "Caso i < index");
+            Debug.Log(index + "Caso i< index");
+            int vPag = index - i;
+            float auxV = 1920 * vPag;
+            desactivarBotones();
+            canvas.LeanMoveX(x + auxV, time).setEase(curve).setOnComplete(() => { if (index == 0 || index == 7) flechasInicioYfin(); else reactivarBotones(); });
+            index = i;
+        }
+        else
+        {
+            flechasInicioYfin();
+            float x = 0;
+            Debug.Log(i + "Caso i >= index");
+            Debug.Log(index + "Caso i >= index");
+            float aux = 1920 * i;
+            desactivarBotones();
+            canvas.LeanMoveX(x - aux, time).setEase(curve).setOnComplete(() => { if (index == 0 || index == 7) flechasInicioYfin(); else reactivarBotones(); });
+            index = i;
+        }
+
+        
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (index == 0)
+       /* if (index == 0)
             botonIzq.SetActive(false);
         else if(index > 0)
             botonIzq.SetActive(true);
@@ -51,7 +114,7 @@ public class CharacterChooseCanvas : MonoBehaviour
         if (index == 7)
             botonDcho.SetActive(false);
         else if(index < 7)
-            botonDcho.SetActive(true);
+            botonDcho.SetActive(true);*/
 
     }
 }
