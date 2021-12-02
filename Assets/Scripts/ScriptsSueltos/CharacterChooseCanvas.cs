5e6bc4ded314dd7d7fa87311bd7f5f5b3f15c29c
Jorge <jgaliano2000@gmail.com>
2021-12-02 18:49:42 +0100

    [TUTORIAL] Creado el tutorial
    El tutorial está creado con placeholders

--- Assets/Scripts/ScriptsSueltos/CharacterChooseCanvas.cs
+++ Assets/Scripts/ScriptsSueltos/CharacterChooseCanvas.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class CharacterChooseCanvas : MonoBehaviour
 {
     public RectTransform canvas;
-
+    public int index;
     public float time = 0.8f;
     public AnimationCurve curve;
+    public GameObject botonIzq, botonDcho;
+    public GameObject[] paginasTutorial;
     // Start is called before the first frame update
     void Start()
     {
+        index = 0;
+        botonDcho.SetActive(true);
+        botonIzq.SetActive(false);
     }
 
     public void MoveRight()
@@ -20,21 +25,33 @@ public class CharacterChooseCanvas : MonoBehaviour
         //
         // Debug.Log("X : "+ xOriginal + " "+mov);
         // LeanTween.moveX(canvas, -mov,time).setEase(curve);
+
         float x = canvas.localPosition.x;
         canvas.LeanMoveX(x+-1920,time).setEase(curve);
+        index++;
     }
 
     public void MoveLeft()
     {
-     
-            
+
         float x = canvas.localPosition.x;
         canvas.LeanMoveX(x+1920,time).setEase(curve);
+        index--;
     }
     
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        if (index == 0)
+            botonIzq.SetActive(false);
+        else if(index > 0)
+            botonIzq.SetActive(true);
+
+        if (index == 7)
+            botonDcho.SetActive(false);
+        else if(index < 7)
+            botonDcho.SetActive(true);
+
     }
 }
