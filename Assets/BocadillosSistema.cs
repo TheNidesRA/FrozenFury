9cc8341bc748a789d4b746567dad1b7454288aaf
Elnidas <pedroydani1@hotmail.com>
2021-12-10 12:48:48 +0100

    Bocadillos del diablo ajustados

---
+++ Assets/BocadillosSistema.cs
@@ -0,0 +1,92 @@
+using System;
+using UnityEngine;
+
+public class BocadillosSistema : MonoBehaviour
+{
+    public static BocadillosSistema _instance;
+
+
+    public Sprite irACaravana;
+    public Sprite irAMuro;
+    public Sprite irACatapulta;
+    public Sprite irACannon;
+
+
+    public Sprite irAICM;
+    public Sprite irAICMSkin1;
+    public Sprite irAICMSkin2;
+    public Sprite irAConeman;
+    public Sprite irAConemanSkin1;
+    public Sprite irAConemanSkin2;
+    public Sprite irATorrine;
+    public Sprite irATorrineSkin1;
+    public Sprite irATorrineSkin2;
+
+
+    private const String NOMBRECANNON = "Ice cream cannon";
+    private const String NOMBRECATAPULTA = "Cherry catapult";
+    private const String NOMBREMURO = "Ice cream wall";
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+
+        else
+        {
+            _instance = this;
+        }
+    }
+
+
+
+
+    public Sprite GetPlayer()
+    {
+        switch (PlayerStats._instance.PlayerStatsSo.PlayerName)
+        {
+            case PlayerStatsSO.playerName.ICM:
+                return irAICM;
+            case PlayerStatsSO.playerName.ICMSkin1:
+                return irAICMSkin1;
+            case PlayerStatsSO.playerName.ICMSkin2:
+                return irAICMSkin2;
+            case PlayerStatsSO.playerName.Coneman:
+                return irAConeman;
+            case PlayerStatsSO.playerName.ConemanSkin1:
+                return irAConemanSkin1;
+            case PlayerStatsSO.playerName.ConemanSkin2:
+                return irAConemanSkin2;
+            case PlayerStatsSO.playerName.Torrine:
+                return irATorrine;
+            case PlayerStatsSO.playerName.TorrineSkin1:
+                return irATorrineSkin1;
+            case PlayerStatsSO.playerName.TorrineSkin2:
+                return irATorrineSkin2;
+        }
+
+        return irAICM;
+    }
+    
+    
+    public Sprite GetSprite(String objetivo)
+    {
+        
+            switch (objetivo)
+            {
+                case NOMBREMURO:
+                    return irAMuro;
+                case NOMBRECANNON:
+                    return irACannon;
+                case NOMBRECATAPULTA:
+                    return irACatapulta;
+                default:
+                    return GetPlayer();
+            }
+        
+     
+        return irACaravana;
+    }
+}
