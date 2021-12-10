522c8292fbeafce40222525386ad0528a2891dde
Elnidas <pedroydani1@hotmail.com>
2021-12-10 13:38:06 +0100

    [HOTFIX][UI]Actualizado el mensaje de cancelar construcción

--- Assets/Scripts/Pause/PauseGame.cs
+++ Assets/Scripts/Pause/PauseGame.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PauseGame : MonoBehaviour
 {
+  
+
   public static void PauseGameAction()
   {
     Time.timeScale = 0;
